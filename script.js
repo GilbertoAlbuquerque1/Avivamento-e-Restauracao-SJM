3669e20f708b379c76211adc33dadb51a99aa68c
Gilberto Albuquerque <gilbertogian.rs@gmail.com>
2026-03-28 00:34:12 +0000

    integração js
    pagina ajuda

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let botao = document.querySelector('.menu-toggle')
+let botaomenu = document.querySelector('.menu-toggle')
 let menu = document.querySelector('.menu')
 
-botao.addEventListener('click', () => {
+botaomenu.addEventListener('click', () => {
     menu.classList.toggle('ativo')
+})
+
+let botaoespiritual = document.querySelector('.apoio_espiritual-toggle')
+let apoioespiritual = document.querySelector('.espiritual')
+let botaosocial = document.querySelector('.apoio_social-toggle')
+let apoiosocial = document.querySelector('.social')
+
+botaoespiritual.addEventListener('click', () => {
+    apoioespiritual.classList.toggle('ativo')
+})
+
+botaosocial.addEventListener('click', () => {
+    apoiosocial.classList.toggle('ativo')
 })
