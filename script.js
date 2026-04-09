863cdb6a6eb6b2276dd72ed2060c2b9fb266c9e4
Gilberto Albuquerque <gilbertogian.rs@gmail.com>
2026-04-09 19:26:52 +0100

    Adição de opções ajuda

--- script.js
+++ script.js
@@ -5,15 +5,33 @@ botaomenu.addEventListener('click', () => {
     menu.classList.toggle('ativo')
 })
 
-let botaoespiritual = document.querySelector('.apoio_espiritual-toggle')
+/* Espiritual */
+let botaoespiritual = document.querySelector('.apoio_espiritual-toggle') 
 let apoioespiritual = document.querySelector('.espiritual')
+
+let botaooracao = document.querySelector('.pedido_oracao-toggle')
+let pedidooracao = document.querySelector('.oracao')
+
+let botaoencontro = document.querySelector('.encontro-toggle') 
+let apoioencontro = document.querySelector('.encontro')
+
+/* Social */
 let botaosocial = document.querySelector('.apoio_social-toggle')
 let apoiosocial = document.querySelector('.social')
+/* Outros */
 let botaooutros = document.querySelector('.outros_assuntos-toggle')
 let apoiooutros = document.querySelector('.outros')
 
 botaoespiritual.addEventListener('click', () => {
     apoioespiritual.classList.toggle('ativo')
+})
+
+botaoencontro.addEventListener('click', () => {
+    apoioencontro.classList.toggle('ativo')
+})
+
+botaooracao.addEventListener('click', () => {
+    pedidooracao.classList.toggle('ativo')
 })
 
 botaosocial.addEventListener('click', () => {
